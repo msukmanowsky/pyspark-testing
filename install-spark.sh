56f7d8018719e2a4dbee29a094664a9aba9cad6f
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2015-11-02 16:43:38 -0500

    Fix environment variables

--- install-spark.sh
+++ install-spark.sh
@@ -5,4 +5,3 @@ HADOOP_VERSION='2.4'
 
 wget http://www.apache.org/dyn/closer.lua/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
 tar -xzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
-export SPARK_HOME=/tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION
