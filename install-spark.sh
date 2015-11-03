e88321c551ddb3e0093031cb3d34a5518f0c0db9
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2015-11-02 20:20:31 -0500

    Fix up spark install

--- install-spark.sh
+++ install-spark.sh
@@ -3,5 +3,5 @@ set -e
 SPARK_VERSION='1.5.1'
 HADOOP_VERSION='2.4'
 
-wget http://www.apache.org/dyn/closer.lua/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
-tar -xzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
